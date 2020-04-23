185cced2c086d612532ad16af72070cc9413289a
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-22 18:17:42 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,88 +1,23 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+var express = require('express');
+var bodyParser = require('body-parser'); 
+var exphbs = require('express-handlebars');
+// let logger = require("morgan");
+var db = require("./models"); 
+// let cheerio = require('cheerio'; 
+// let mongoose = require('mongoose';
+// let db from "./models"; 
 
-var axios = require("axios");
-var cheerio = require("cheerio");
+var PORT = process.env.PORT || 3000; 
+var app = express(); 
 
-var db = require("./models");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+require("./controllers/webScrapperController.js")(app);
 
-mongoose.connect(MONGODB_URI);
-
-app.get("/scrape", function(req, res) {
-    axios.get("https://www.nba.com/news").then(function(response) {
-      var $ = cheerio.load(response.data);
-
-      $("h5").each(function(i, element) {
-        var result = {};
-
-        result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-        db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    });
-
-    res.send("Scrape Complete!");
-  });
-});
-
-app.get("/articles", function(req, res) {
-      db.Article.find()
-        .then(function(dbPopulate) {
-          res.json(dbPopulate);
-        })
-        .catch(function(err) {
-         res.json(err);
-        });
-});
-
-app.get("/articles/:id", function(req, res) {
-    db.Article.findById(req.params.id)
-    .populate("note")
-    .then(function(dbPopulate) {
-      res.json(dbPopulate);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-      .then(function(dbPopulate) {
-        
-        return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbPopulate._id } }, { new: true });
-      })
-      .then(function(dbPopulate) {
-        res.json(dbPopulate);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-  
-  app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
-  
+app.listen(PORT, ()=>{
+    console.log(`App listening on PORT ${PORT}`);
+})
