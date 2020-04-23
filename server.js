90c9d38b0467c644903e159c934db6553c0c29e1
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-22 19:12:26 -0600

    updated server.js file

--- server.js
+++ server.js
@@ -1,23 +1,95 @@
-var express = require('express');
-var bodyParser = require('body-parser'); 
+var mongojs = require("mongojs");
+var cheerio = require("cheerio");
+var axios = require("axios");
+var express = require("express");
 var exphbs = require('express-handlebars');
-// let logger = require("morgan");
-var db = require("./models"); 
-// let cheerio = require('cheerio'; 
-// let mongoose = require('mongoose';
-// let db from "./models"; 
 
-var PORT = process.env.PORT || 3000; 
-var app = express(); 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var app = express();
+var PORT = process.env.PORT || 3000
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./controllers/webScrapperController.js")(app);
+var collections = ["scrapedData"];
 
-app.listen(PORT, ()=>{
-    console.log(`App listening on PORT ${PORT}`);
+var db = mongojs(process.env.MONGODB_URI || 'scraper', collections);
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
+
+app.get("/", function(req, res) {
+  res.render("index");
 })
+
+app.get("/scrape", function (req, res) {
+
+  db.scrapedData.drop()
+
+  axios
+    .get("https://kotaku.com/")
+    .then(function (response) {
+      var $ = cheerio.load(response.data);
+
+      var results = [];
+
+      $("article").each(function (i, element) {
+        var title = $(element)
+          .find("h1")
+          .children("a")
+          .text();
+        var link = $(element)
+          .find("h1")
+          .children("a")
+          .attr("href");
+        var image = $(element)
+          .find("source")
+          .attr("data-srcset");
+        var summary = $(element)
+          .find("p").text();
+
+       if (title && link && image && summary) {
+        db.scrapedData.insert({
+          title: title,
+          link: link,
+          image: image,
+          summary: summary
+        }, 
+        function(err, inserted) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log("scrapedData")
+            console.log(inserted);
+          }
+        });
+      }
+      });
+    })
+});
+
+app.get("/all", function (req, res) {
+  db.scrapedData.find({}, function (err, found) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(found)
+    }
+  });
+});
+
+app.get("/title", function(req, res) {
+ 
+  db.scrapedData.find().sort({ title: 1 }, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.send(found);
+    }
+  });
+});
+
+app.listen(PORT, function () {
+  console.log("App running on port 3000!");
+});
