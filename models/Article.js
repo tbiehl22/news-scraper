ae75b326f11cce969cddcab257c3d7b55b236b71
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-22 18:35:21 -0600

    updated Article.js file

--- models/Article.js
+++ models/Article.js
@@ -1,23 +1,38 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; 
+var ArticleSchema = new Schema({ 
 
-var Schema = mongoose.Schema;
-
-var ArticleSchema = new Schema({
-  title: {
+  headline: {
     type: String,
     required: true
   },
-  link: {
+
+  summary: {
     type: String,
     required: true
   },
-  
-  note: {
+
+  url: {
+    type: String,
+    required: true
+  },
+
+  imageURL: {
+    type: String,
+    required: true
+  },
+
+  slug: {
+    type: String
+  },
+
+  note: [{
     type: Schema.Types.ObjectId,
     ref: "Note"
-  }
-});
+  }]
+
+}); 
 
 var Article = mongoose.model("Article", ArticleSchema);
 
-module.exports = Article;
+module.exports = Article; 
